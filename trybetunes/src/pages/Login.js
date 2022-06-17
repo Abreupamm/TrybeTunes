a3566efea39fc3c16e52861ce09edae4e88ac5c0
PAMELA <abreupamm@gmail.com>
2022-06-17 20:21:44 -0300

    componentes, rotas e pagina de lodin criados

---
+++ trybetunes/src/pages/Login.js
@@ -0,0 +1,83 @@
+import './Login.css';
+import React from 'react';
+import PropTypes from 'prop-types';
+// import { createUser } from '../services/userAPI';
+// import Loading from '../components/Loading';
+
+class Login extends React.Component {
+  state = {
+    userName: '',
+    disabled: true,
+    isLoggedIn: false,
+  }
+
+  isButtonDisabled = () => {
+    const { userName } = this.state;
+    if (userName.length > 2) {
+      return this.setState({ disabled: false });
+    }
+    return this.setState({ disabled: true });
+  }
+
+  handleOnChange = (event) => {
+    const { value } = event.target;
+    this.setState({ userName: value }, () => {
+      this.isButtonDisabled();
+    });
+  }
+
+  handleOnClick = async () => {
+    // const { userName } = this.state;
+    const { history } = this.props;
+    this.setState({ isLoggedIn: true });
+    // await createUser({ name: userName });
+    this.setState({ isLoggedIn: false });
+    history.push('/search');
+  }
+
+  render() {
+    const { disabled } = this.state;
+    // if (isLoggedIn) {
+    //   return <Loading />;
+    // }
+    return (
+      <div className="login-page">
+        <div className="title">
+          <h1>Trybe</h1>
+          <img alt="logo" src="https://cdn-icons-png.flaticon.com/512/5885/5885170.png" />
+          <h2>Tunes</h2>
+        </div>
+        <div
+          data-testid="page-login"
+          className="container-login"
+        >
+          <form>
+            <input
+              data-testid="login-name-input"
+              type="text"
+              onChange={ this.handleOnChange }
+            />
+            <br />
+            <button
+              type="button"
+              data-testid="login-submit-button"
+              disabled={ disabled }
+              onClick={ this.handleOnClick }
+            >
+              Entrar
+            </button>
+          </form>
+          <div className="page-login-gif" />
+        </div>
+      </div>
+    );
+  }
+}
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+export default Login;
---
+++ trybetunes/src/pages/Profile.js/Search.js
@@ -0,0 +1,90 @@
+import React from 'react';
+// import Header from '../components/Header';
+// import searchAlbumsAPI from '../services/searchAlbumsAPI';
+// import Loading from '../components/Loading';
+// import ResultAlbuns from './ResultAlbuns';
+
+class Search extends React.Component {
+  state = {
+    artistName: '',
+    disabled: true,
+    click: false,
+    returnArtist: false,
+    listAlbuns: [],
+    artistAlbum: '',
+  }
+
+//   isButtonDisabld = () => {
+//     const { artistName } = this.state;
+//     if (artistName.length > 1) {
+//       return this.setState({ disabled: false });
+//     }
+//     return this.setState({ disabled: true });
+//   }
+
+//   handleOnChange = (event) => {
+//     const { value } = event.target;
+//     this.setState({ artistName: value }, () => {
+//       this.isButtonDisabld();
+//     });
+//   }
+
+//   handleOnClick = async (event) => {
+//     const { name } = event.target;
+//     const { artistName } = this.state;
+//     this.setState({ click: true });
+//     const albuns = await searchAlbumsAPI(name);
+//     this.setState({ listAlbuns: albuns });
+//     this.setState({ click: false });
+//     this.setState({ returnArtist: true });
+//     this.setState({ artistAlbum: artistName });
+//     this.setState({ artistName: '' });
+//   }
+
+  render() {
+    // const {
+    //   disabled,
+    //   artistName,
+    //   click,
+    //   returnArtist,
+    //   artistAlbum,
+    //   listAlbuns,
+    // } = this.state;
+    // if (click) {
+    //   return <Loading />;
+    // }
+    return (
+      <div data-testid="page-search">
+        Search
+        {/* <Header />
+        {click ? <Loading /> : (
+          <div className="container-search">
+            <input
+              value={ artistName }
+              data-testid="search-artist-input"
+              type="text"
+              onChange={ this.handleOnChange }
+            />
+            <button
+              name={ artistName }
+              data-testid="search-artist-button"
+              type="button"
+              disabled={ disabled }
+              onClick={ this.handleOnClick }
+            >
+              Pesquisar
+            </button>
+          </div>
+        )}
+        {
+          returnArtist && <ResultAlbuns
+            albuns={ listAlbuns }
+            artist={ artistAlbum }
+          />
+        } */}
+      </div>
+    );
+  }
+}
+
+export default Search;
